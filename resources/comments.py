945f760d3e8f8b81dd18c4627e42cb024e24df76
heyawmeteke <heyawmeteke@gmail.com>
2016-11-24 14:14:52 -0500

    added to comment api

--- resources/comments.py
+++ resources/comments.py
@@ -1,3 +1,45 @@
 import DB
 from flask_restful import Resource
+from response import ResponseAPI
 
+class Comment(Resouce):
+    def post(self):
+        data = {}
+        message = ""
+        error = False
+        
+        try:
+            data = db.Comment.create(request.get_json(force=True)).json()
+            
+        except Exception as e:
+            message = str(e)
+            error = True
+            
+        finally:
+            if error is True:
+                DB.db.session.rollback()
+            ResponseAPI('Comment', 'post', data=data, message=message, error=error).json()
+            
+  
+class Comments(Resource):
+    def get(self, id):
+        data = {}
+        message = ""
+        error = False
+    
+    try:
+        data = DB.Comment.find_model_by_id(id).json()
+    
+    except AttributeError:
+        
+        message = "Cannot not be set"
+        error = True
+        
+    except Exception as e:
+        message = str(e)
+        error = True
+     
+    finally:
+        if error is True:
+            DB.db.session.rollback()
+        ResponseAPI('Comments', 'get', data=data, message=message, error=error)
